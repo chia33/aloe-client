9bacd6714061f004a3c90966c7c0d6bb532e555b
chia33 <dchia33@gmail.com>
2019-02-23 14:28:27 +0800

    加入antd，并对less支持

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './app.less';
+import {Button} from 'antd';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>hello</div>
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
